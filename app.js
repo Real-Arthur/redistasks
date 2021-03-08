48463705343fbc86f10d9224c89c6001e01129e4
Arthur Carson <rthrcrsn@gmail.com>
2021-03-07 18:56:32 -0600

    added next scheduled call post

--- app.js
+++ app.js
@@ -29,10 +29,14 @@ app.get('/', function(req,res) {
   const title = 'Task List';
 
   client.lrange('tasks', 0, -1, function(err, reply) {
-    res.render('index', {
-    title: title,
-    tasks: reply
-  })  
+    client.hgetall('call', function(err, call){
+      res.render('index', {
+      title: title,
+      tasks: reply,
+      call: call
+      })  
+    })
+
   });
 })
 
@@ -60,6 +64,23 @@ app.post('/task/delete', function(req, res){
         });
       }
     }
+    res.redirect('/')
+  })
+})
+
+app.post('/call/add', function(req, res){
+  const newCall = {};
+
+  newCall.name = req.body.name;
+  newCall.company = req.body.company;
+  newCall.phone = req.body.phone;
+  newCall.time = req.body.time;
+
+  client.hmset('call', ['name', newCall.name, 'company', newCall.company, 'phone', newCall.phone, 'time', newCall.time], function(err, reply){
+    if(err){
+      console.log(err);
+    }
+    console.log(reply);
     res.redirect('/')
   })
 })
